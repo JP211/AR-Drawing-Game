7c82d6a264eb20c8de3cdb1c95ead2269a01f10d
Jessica Peters <jessicapeters@Jessicas-Air.attlocal.net>
2018-04-30 03:58:00 -0500

    Initial setup for View Controller

---
+++ AR Drawing/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  AR Drawing
+//
+//  Created by Jessica Peters on 4/30/18.
+//  Copyright © 2018 Jessica Peters. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.showsStatistics = true
+        self.sceneView.session.run(configuration)
+        self.sceneView.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        print("rendering")
+    }
+
+
+}
+
